8ccf1336c6504236521c644232b077e835fccb2c
Tanaka Akira <akr@fsij.org>
2010-06-12 02:01:55 +0900

    autoconfed.

--- localtime.c
+++ localtime.c
@@ -1,3 +1,4 @@
+#include "config.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -12,12 +13,21 @@ int main(int argc, char *argv[])
   tmp = localtime(&t);
   if (tmp == NULL) { fprintf(stderr, "localtime error\n"); exit(1); }
 
-  printf("localtime: %ld -> %d-%02d-%02d %02d:%02d:%02d %s %s (%ld) %ld\n",
+  printf("localtime: %ld -> %d-%02d-%02d %02d:%02d:%02d %s",
     t,
     tmp->tm_year + 1900, tmp->tm_mon + 1, tmp->tm_mday,
     tmp->tm_hour, tmp->tm_min, tmp->tm_sec,
-    tmp->tm_isdst > 0 ? "dst" : tmp->tm_isdst == 0 ? "std" : "unknown",
-    tmp->tm_zone, tmp->tm_gmtoff);
+    tmp->tm_isdst > 0 ? "dst" : tmp->tm_isdst == 0 ? "std" : "unknown");
+
+#ifdef HAVE_STRUCT_TM_TM_ZONE
+  printf(" zone=%s", tmp->tm_zone);
+#endif
+
+#ifdef HAVE_STRUCT_TM_TM_GMTOFF
+  printf(" gmtoff=%ld", tmp->tm_gmtoff);
+#endif
+
+  printf("\n");
 
   return 0;
 }
