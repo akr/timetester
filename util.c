aca17a492c77a3da85b1fa2962cbbdf3062fd875
Tanaka Akira <akr@fsij.org>
2010-06-12 11:44:11 +0900

    refined.

--- util.c
+++ util.c
@@ -3,33 +3,34 @@
 int timenum_parse(const char *str, time_t *res)
 {
 #if SIZEOF_TIME_T <= SIZEOF_LONG
-#   define CONVTYPE long
-#   define CONVFUN_SIGNED(s,e,b) strtol(s,e,b)
-#   define CONVFUN_UNSIGNED(s,e,b) strtoul(s,e,b)
+#   ifdef TIME_IS_SIGNED
+#       define CONVTYPE long
+#       define CONVFUN(s,e,b) strtol(s,e,b)
+#   else
+#       define CONVTYPE unsigned long
+#       define CONVFUN(s,e,b) strtoul(s,e,b)
+#   endif
 #elif SIZEOF_TIME_T <= SIZEOF_LONG_LONG
-#   define CONVTYPE long long
-#   define CONVFUN_SIGNED(s,e,b) strtol(s,e,b)
-#   define CONVFUN_UNSIGNED(s,e,b) strtoul(s,e,b)
+#   ifdef TIME_IS_SIGNED
+#       define CONVTYPE long long
+#       define CONVFUN(s,e,b) strtoll(s,e,b)
+#   else
+#       define CONVTYPE unsigned long long
+#       define CONVFUN(s,e,b) strtoull(s,e,b)
+#   endif
 #else
 # error sizeof(time_t) unexpected.
 #endif
 
-#ifdef TIME_IS_SIGNED
   CONVTYPE v;
   errno = 0;
-  v = CONVFUN_SIGNED(str, NULL, 0);
+  v = CONVFUN(str, NULL, 0);
   if (errno)
     return -1;
   *res = v;
   return 0;
-#else
-  unsigned CONVTYPE v;
-  errno = 0;
-  v = CONVFUN_UNSIGNED(str, NULL, 0);
-  if (errno)
-    return -1;
-  *res = v;
-  return 0;
-#endif
+
+#undef CONVTYPE
+#undef CONVFUN
 }
 
