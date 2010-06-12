c24380ad15ea09f9e63dc414af80d3828135c6d8
Tanaka Akira <akr@fsij.org>
2010-06-13 00:02:47 +0900

    format refined.

--- mktime.c
+++ mktime.c
@@ -1,23 +1,11 @@
 #include "timetester.h"
 
-char *weekday(int wday)
-{
-  switch (wday) {
-    case 0: return "Sun";
-    case 1: return "Mon";
-    case 2: return "Tue";
-    case 3: return "Wed";
-    case 4: return "Thu";
-    case 5: return "Fri";
-    case 6: return "Sat";
-    default: return "unexpected-weekday-number";
-  }
-}
-
 int main(int argc, char *argv[])
 {
   time_t t;
   struct tm tmarg, tmp;
+  long long y;
+  int ret;
 
   if (argc <= 1) {
     fprintf(stderr, "usage: mktime year [mon [day [hour [min [sec [isdst]]]]]]\n");
@@ -35,17 +23,56 @@ int main(int argc, char *argv[])
   tmp = tmarg;
 
   t = mktime(&tmp);
-  if (t == (time_t)-1) { fprintf(stderr, "mktime error (-1 is returned)\n"); exit(1); }
+  if (t == (time_t)-1) { fprintf(stderr, "mktime returns -1 (error)\n"); exit(1); }
 
-  printf("mktime: %d-%02d-%02d %02d:%02d:%02d (isdst=%d) -> %d-%02d-%02d %02d:%02d:%02d %s (isdst=%d yday=%d) %ld\n",
-    tmarg.tm_year + 1900, tmarg.tm_mon + 1, tmarg.tm_mday,
-    tmarg.tm_hour, tmarg.tm_min, tmarg.tm_sec,
-    tmarg.tm_isdst,
-    tmp.tm_year + 1900, tmp.tm_mon + 1, tmp.tm_mday,
-    tmp.tm_hour, tmp.tm_min, tmp.tm_sec,
-    weekday(tmp.tm_wday),
-    tmp.tm_isdst, tmp.tm_yday,
-    t);
+  y = 1900 + (long long)tmarg.tm_year;
+  printf("%lld-%02d-%02d %02d:%02d:%02d",
+    y, tmarg.tm_mon + 1, tmarg.tm_mday,
+    tmarg.tm_hour, tmarg.tm_min, tmarg.tm_sec);
+
+  printf(" %s",
+    0 < tmarg.tm_isdst ? "dst" : tmarg.tm_isdst == 0 ? "std" : "std|dst");
+
+  printf(" ->");
+
+  printf(" %"PRIdTIME, t);
+
+  y = 1900 + (long long)tmp.tm_year;
+  printf(" %lld-%02d-%02d %02d:%02d:%02d",
+    y, tmp.tm_mon + 1, tmp.tm_mday,
+    tmp.tm_hour, tmp.tm_min, tmp.tm_sec);
+
+#ifdef HAVE_STRUCT_TM_TM_GMTOFF
+  {
+    char gmtoff_buf[16];
+    ret = format_gmtoff(gmtoff_buf, sizeof(gmtoff_buf), tmp.tm_gmtoff);
+    if (0 < ret && ret < sizeof(gmtoff_buf))
+      printf(" %s", gmtoff_buf);
+    else
+      printf(" gmtoff=%ld", tmp.tm_gmtoff);
+  }
+#endif
+
+  printf(" %s",
+    0 < tmp.tm_isdst ? "dst" : tmp.tm_isdst == 0 ? "std" : "std|dst");
+
+#ifdef HAVE_STRUCT_TM_TM_ZONE
+  printf(" (%s)", tmp.tm_zone);
+#endif
+
+  printf(" yday=%d",
+    tmp.tm_yday);
+
+  {
+    char buf[16];
+    ret = format_wday(buf, sizeof(buf), tmp.tm_wday);
+    if (0 < ret && ret < sizeof(buf))
+      printf(" %s", buf);
+    else
+      printf(" wday=%d", tmp.tm_wday);
+  }
+
+  printf("\n");
 
   return 0;
 }
