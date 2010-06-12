a11a8c3cf022c39f3d0bf3e026fb80d7d6750e19
Tanaka Akira <akr@fsij.org>
2010-06-12 10:24:15 +0900

    show sizeof(tm_year).

--- info.c
+++ info.c
@@ -1,10 +1,11 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+#include "timetester.h"
 
 int main(int argc, char *argv[])
 {
+  struct tm tm_var;
+
   printf("sizeof(time_t)=%d\n", (int)sizeof(time_t));
+  printf("sizeof(struct tm.tm_year)=%d\n", (int)sizeof(tm_var.tm_year));
   printf("sizeof(int)=%d\n", (int)sizeof(int));
   printf("sizeof(long)=%d\n", (int)sizeof(long));
   return 0;
